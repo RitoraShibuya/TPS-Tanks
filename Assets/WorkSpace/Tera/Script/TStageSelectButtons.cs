ce83f163acc6e3bb4f9c76aa153f734e0599e648
DESKTOP-RRBSBA0\PC_User <mizuki.terasawa@gmail.com>
2026-07-30 16:50:56 +0900

    寺澤　Seen用Scriptを追加

---
+++ Assets/WorkSpace/Tera/Script/TStageSelectButtons.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class StageSelectButtons : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TOnStage1Click()
+    {
+
+
+    }
+
+    public void TOnStage2Click()
+    {
+
+
+    }
+    public void TOnStage3Click()
+    {
+
+
+    }
+    public void TOnStage4Click()
+    {
+
+
+    }
+
+    public void TOnTutorialClick()
+    {
+
+    }
+
+}
